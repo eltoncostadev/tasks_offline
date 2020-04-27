72d9ecf4324048d2824ef677f9c9de34027dafa9
eltoncostadev <61428229+eltoncostadev@users.noreply.github.com>
2020-04-27 13:59:48 -0300

    Include
    Include

---
+++ src/commonStyles.js
@@ -0,0 +1,10 @@
+const newLocal = 'Century';
+export default{
+    fontFamily: newLocal,
+    colors: {
+        secondary: '#FFF',
+        mainText: '#A5A3A3',
+        subText: '#555',
+        today: '#B13B44'
+    }
+}
